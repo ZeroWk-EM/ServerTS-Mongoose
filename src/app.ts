57f3917843b13bde290c149a1007879e81624d5b
Emanuele Moncada <em@zerowk.it>
2023-04-05 18:02:03 +0200

    feat(app): update routes dependance

--- src/app.ts
+++ src/app.ts
@@ -12,11 +12,19 @@ app.get("/", (req, res) => {
   res.status(200).json({ status_code: 200, message: "Server is started" });
 });
 
+import login from "./routes/login.routes";
+import me from "./routes/me.routes";
+import signup from "./routes/signup.routes";
 import user from "./routes/user.routes";
+import validate from "./routes/validate.routes";
 
+app.use("/login", login);
+app.use("/me", me);
+app.use("/signup", signup);
 app.use("/users", user);
+app.use("/validate", validate);
 
 app.listen(port, () => {
-  console.log(`\x1b[32m[Server] Server Connected on port [${port}]\x1b[0m`);
+  console.log(`\x1b[32m[Server] Server Connected on port ${port}\x1b[0m`);
   connectToMongoDB();
 });
