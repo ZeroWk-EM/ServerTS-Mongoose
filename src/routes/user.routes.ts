e29e1a2abf4d61cfbc51030fba0183c1c22b56dc
Emanuele Moncada <em@zerowk.it>
2023-04-05 17:45:50 +0200

    feat(user.routes): add user routes

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import { IUser } from "../interfaces/user.interface";
+import User from "../models/user.model";
+
+dotenv.config();
+const router = express.Router();
+router.use(express.json());
+
+router.get("/", async ({ query }, res) => {
+  try {
+    const findAll = await User.find(query);
+    res.status(200).json(findAll);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export default router
