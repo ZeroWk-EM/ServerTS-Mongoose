507d99dfcf98cd22f568f5f9a3d37349ba44ceff
Emanuele Moncada <em@zerowk.it>
2023-04-06 12:04:24 +0200

    feat(routes): add login and signup endpoint

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,12 +1,44 @@
 import express from "express";
 import dotenv from "dotenv";
 import { IUser } from "../interfaces/user.interface";
+import { body } from "express-validator";
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 import User from "../models/user.model";
+import { checkErrors } from "../middlewares/validation.middleware";
+import { use } from "chai";
 
 dotenv.config();
+const saltRounds = Number(process.env.SALT);
 const router = express.Router();
 router.use(express.json());
 
-router.post("/", async ({ query }, res) => {});
+router.post(
+  "/",
+  body("email").isEmail(),
+  body("password").isLength({ min: 8 }).notEmpty(),
+  checkErrors,
+  async ({ body }, res) => {
+    try {
+      const user = await User.findOne({
+        email: body.email,
+        verify: { $exists: false },
+      });
+      if (user) {
+        const comparePassword = await bcrypt.compare(
+          body.password,
+          user.password
+        );
+        if (comparePassword) {
+          return res.status(200).json({ message: "Auth Successful" });
+        }
+        return res.status(401).json({ message: "Wrong password" });
+      }
+      res.status(401).json({ message: "Verify Token please..." });
+    } catch (errors) {
+      res.status(400).json({ message: "Error to create user", error: errors });
+    }
+  }
+);
 
 export default router;
--- src/routes/signup.routes.ts
+++ src/routes/signup.routes.ts
@@ -1,12 +1,46 @@
 import express from "express";
 import dotenv from "dotenv";
 import { IUser } from "../interfaces/user.interface";
+import { body } from "express-validator";
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 import User from "../models/user.model";
+import { checkErrors, uniqueEmail } from "../middlewares/validation.middleware";
 
 dotenv.config();
+const saltRounds = Number(process.env.SALT);
 const router = express.Router();
 router.use(express.json());
 
-router.post("/", async ({ query }, res) => {});
+router.post(
+  "/",
+  body("email").isEmail(),
+  body("name").notEmpty(),
+  body("surname").notEmpty(),
+  body("password").isLength({ min: 8 }).notEmpty(),
+  checkErrors,
+  uniqueEmail,
+  async ({ body }, res) => {
+    const userBody: IUser = body;
+    try {
+      const user = await User.create({
+        name: userBody.name,
+        surname: userBody.surname,
+        email: userBody.email,
+        password: await bcrypt.hash(userBody.password, saltRounds),
+        verify: uuidv4(),
+      });
+      if (user) {
+        res.status(201).json({
+          name: user.name,
+          surname: user.surname,
+          email: user.email,
+        });
+      }
+    } catch (errors) {
+      res.status(400).json({ message: "Error to create user", error: errors });
+    }
+  }
+);
 
 export default router;
